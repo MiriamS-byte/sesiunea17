4515871656b4172e800e195b78f1a445c8ce6dca
MiriamS-byte <schataram161200@gmail.com>
2024-10-24 19:48:19 +0300

    Display 1st question in quiz

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 class Quiz {
-    constructor(questions){}
+    constructor(questions){
+        this.questions = questions;
+        this.question = document.getElementById("question");
+        this.answerButtons = document.getElementById("answer-buttons");
+        this.feedback = document.getElementById("feedback");
+        this.nextButton = document.getElementById("next-button");
+    }
+
+    init(){
+        const question = this.questions[0];
+        this.question.textContent = question.question;
+        this.answerButtons.innerHTML = "";
+        for(const answer of question.answers){
+            const li = document.createElement("li");
+            // li.innerHTML = `<button class="btn">${option.text}</button>`;
+            const btn = document.createElement("button");
+            btn.className = "btn";
+            btn.textContent = answer.text;
+            li.appendChild(btn);
+            this.answerButtons.appendChild(li);
+        }
+    }
 }
+
+document.addEventListener(`DOMContentLoaded`, async () => {
+    const fileData = await fetch("./questions.json");
+    const questions = await fileData.json();
+    console.log(questions);
+
+    const quiz = new Quiz(questions);
+    quiz.init();
+})
